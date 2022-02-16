eb86bcd56fbd829b40035df9cf830a597c937582
Igor Bayerl <bayerl.igor@gmail.com>
2022-02-15 23:03:52 -0300

    restyle mobile menu

--- components/BottomMenu.tsx
+++ components/BottomMenu.tsx
@@ -10,15 +10,16 @@ const BottomMenu: React.FC<IProps> = ({ scrollDirection }) => {
   useEffect(() => {
     setVisibleClass(scrollDirection === 0 ? "" : "hide-bottom-menu");
   }, [scrollDirection]);
+
   return (
     <nav
-      className={`${visibleClass} bottom-menu flex sm:hidden mx-10 z-50 my-10 h-16 justify-evenly bg-slate-100 rounded-full fixed bottom-0 left-0 right-0`}
+      className={`${visibleClass} bottom-menu flex sm:hidden mx-10 z-50 shadow-md my-10 h-16 justify-evenly bg-white rounded-full fixed bottom-0 `}
     >
       <div className="flex items-center">
-        <ChatIcon className="cursor-pointer h-10 w-10 text-gray-500" />
+        <ChatIcon className="cursor-pointer  h-12 w-12 p-2 m-6 text-gray-500" />
       </div>
       <div className="flex items-center">
-        <SearchIcon className="cursor-pointer h-10 w-10 text-gray-500" />
+        <SearchIcon className="cursor-pointer h-12 w-12 p-2 m-6 text-gray-500" />
       </div>
     </nav>
   );
