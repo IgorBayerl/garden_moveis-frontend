77ce0b5998b37c68c8e532d72b26b7e2ba086d3b
Igor Bayerl <bayerl.igor@gmail.com>
2022-03-20 10:48:40 -0300

    footer margin top

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import { FiMail, FiHome } from "react-icons/fi";
 
 const Footer: React.FC = () => {
   return (
-    <div className="w-full md:flex bg-white test-shaddow  footerContainer justify-center ">
+    <div className="w-full md:flex bg-white test-shaddow mt-2 footerContainer justify-center ">
       <nav className="flex w-full flex-col md:flex-row  max-w-[1400px] navbar navbar-expand-lg justify-center items-center sm:justify-between  px-10 py-5 navbar-light ">
         <div className="flex items-center ">
           <span>
