63eb23342c7eae626e184d541b48a13042ec9806
Igor Bayerl <bayerl.igor@gmail.com>
2022-03-31 22:12:27 -0300

    meta description

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,8 +8,11 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         {/* <link rel="icon" href="/images/favicon.png" /> */}
         {/* <meta name="google-site-verification" content="rL0WQyAInIZEBhPV5C40ssChKzgnRDx3heD93UtoJfs" /> */}
-        <title>Garden Moveis</title>
-        <meta name="description" content="fabrica de moveis de madeira" />
+        <title>Garden Moveis - Fabrica de Moveis - São Bento do Sul</title>
+        <meta
+          name="description"
+          content="A Garden Móveis é uma fabrica de moveis de madeira, localizada em São Bento do Sul, especializada em moveis para jardim e área externa."
+        />
         <meta
           name="keywords"
           content="Garden, Wood, Moveis, Furniture, Madeira"
